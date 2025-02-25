3f92843f9aaf3cd0a780e9958efc0afd8ce40df7
Provost Simon <simon1.provost@epitech.eu>
2025-02-24 21:29:55 -0500

    feat(mixins): add search/loader/display mixins

---
+++ src/auctus_search/mixins/__init__.py
@@ -0,0 +1,9 @@
+from .display import AuctusSearchDisplayMixin
+from .loader import AuctusSearchLoaderMixin
+from .search import AuctusSearchMixin
+
+__all__ = [
+    "AuctusSearchDisplayMixin",
+    "AuctusSearchLoaderMixin",
+    "AuctusSearchMixin",
+]
