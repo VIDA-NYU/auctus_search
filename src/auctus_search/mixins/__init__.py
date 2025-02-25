41eca3e09d1913a1d4fcd2fa7c859484565bc573
Provost Simon <simon1.provost@epitech.eu>
2025-02-25 13:10:05 -0500

    feat(mixins): add profile from DataProfileVis mixin

--- src/auctus_search/mixins/__init__.py
+++ src/auctus_search/mixins/__init__.py
@@ -1,9 +1,11 @@
 from .display import AuctusSearchDisplayMixin
 from .loader import AuctusSearchLoaderMixin
 from .search import AuctusSearchMixin
+from .profile import DataProfileViewerMixin
 
 __all__ = [
     "AuctusSearchDisplayMixin",
     "AuctusSearchLoaderMixin",
     "AuctusSearchMixin",
+    "DataProfileViewerMixin",
 ]
